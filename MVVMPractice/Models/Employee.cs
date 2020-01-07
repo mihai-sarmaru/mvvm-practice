b893ad082c348df4ab04e66a699ea06fe045b2f0
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-07 09:35:25 +0200

    Add Employee model

---
+++ MVVMPractice/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVMPractice.Models {
+    public class Employee {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Experience { get; set; }
+    }
+}
