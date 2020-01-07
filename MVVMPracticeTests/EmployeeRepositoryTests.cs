0a1215a6c13e9eb89d733c65bf83147537776514
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-07 10:21:42 +0200

    Add EmployeeRepository test class with GetEmployeeJsonPath unit test

---
+++ MVVMPracticeTests/EmployeeRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMPractice.Services;
+
+namespace MVVMPracticeTests {
+    [TestClass]
+    public class EmployeeRepositoryTests {
+
+        [TestMethod]
+        public void TestGetEmployeeJsonPath() {
+            string expected = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "json", "Employee.json");
+            string result = EmployeeRepository.GetEmployeeJsonPath();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
