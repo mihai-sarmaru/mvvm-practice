b08a4d377d2eded36154e47afc3127d81a4a42c6
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-07 10:54:45 +0200

    Add EmployeeForm View

---
+++ MVVMPractice/Views/EmployeeForm.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MVVMPractice.Views {
+    /// <summary>
+    /// Interaction logic for EmployeeForm.xaml
+    /// </summary>
+    public partial class EmployeeForm : UserControl {
+        public EmployeeForm() {
+            InitializeComponent();
+        }
+    }
+}
