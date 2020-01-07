d56f73dab9b7799de7b1dcb5c9fdb5d1f8c4ca52
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-07 13:28:35 +0200

    Add EmployeeFormViewModel with generic methods

---
+++ MVVMPractice/ViewModels/EmployeeFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using MVVMPractice.Models;
+
+namespace MVVMPractice.ViewModels {
+    public class EmployeeFormViewModel {
+        public Employee Employee { get; set; }
+        public ICommand SaveCommand { get; set; }
+
+        public EmployeeFormViewModel() {
+            SaveCommand = new RelayCommand(SaveEmployee);
+        }
+
+        public void SaveEmployee() {
+        }
+    }
+}
