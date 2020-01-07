9b77ad17526d68aa3b5643dd4f070f5155ba1b32
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-07 10:21:04 +0200

    Add EmployeeRepository with GetEmployeeJsonPath method

---
+++ MVVMPractice/Services/EmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace MVVMPractice.Services {
+    public static class EmployeeRepository {
+
+        private const string JSON_FOLDER = "json";
+        private const string JSON_EMPLOYEE_FILENAME = "Employee.json";
+
+        public static string GetEmployeeJsonPath() {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, JSON_FOLDER, JSON_EMPLOYEE_FILENAME);
+        }
+
+    }
+}
