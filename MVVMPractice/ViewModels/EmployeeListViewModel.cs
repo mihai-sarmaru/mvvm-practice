7e2ff78cede021250e5958f7187939141a1fce91
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-09 09:54:51 +0200

    Add EmployeeListView to MainWindow

---
+++ MVVMPractice/ViewModels/EmployeeListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MVVMPractice.Models;
+using MVVMPractice.Services;
+
+namespace MVVMPractice.ViewModels {
+    public class EmployeeListViewModel {
+        public List<Employee> EmployeeList { get; set; }
+
+        public EmployeeListViewModel() {
+            EmployeeList = EmployeeRepository.GetEmployeeList();
+        }
+    }
+}
