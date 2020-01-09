2885df0b1c40a6f1fc98fb308ab020d07c8a3048
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-09 14:27:50 +0200

    Add button to remove employee from list

--- MVVMPractice/ViewModels/EmployeeListViewModel.cs
+++ MVVMPractice/ViewModels/EmployeeListViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 using MVVMPractice.Messages;
 using MVVMPractice.Models;
@@ -10,16 +13,23 @@ namespace MVVMPractice.ViewModels {
     public class EmployeeListViewModel {
         public ObservableCollection<Employee> EmployeeList { get; set; }
         public ICommand ListSelectionChanged { get; set; }
+        public ICommand RemoveEmployeeCommand { get; set; }
 
         public EmployeeListViewModel() {
             EmployeeList = new ObservableCollection<Employee>(EmployeeRepository.GetEmployeeList());
             Messenger.Default.Register<UpdateEmployeeListMessage>(this, UpdateEmployeeList);
 
             ListSelectionChanged = new RelayCommand<Employee>((emp) => EmployeeListSelectionChanged(emp));
+            RemoveEmployeeCommand = new RelayCommand<string>((empID) => RemoveEmployeeFromList(empID));
+        }
 
         private void EmployeeListSelectionChanged(Employee employee) {
             Messenger.Default.Send(new UpdateEmployeeFormMessage { Employee = employee });
         }
+
+        private void RemoveEmployeeFromList(string employeeID) {
+            Employee employeeToRemove = EmployeeList.Single(empID => empID.ID == employeeID);
+            EmployeeList.Remove(employeeToRemove);
         }
 
         private void UpdateEmployeeList(UpdateEmployeeListMessage message) {
