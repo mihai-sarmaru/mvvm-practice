e5212777184cdbe824dbf06a7e059fb966979a7f
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-09 10:58:49 +0200

    Add Employee model unit tests

---
+++ MVVMPracticeTests/ModelTests/EmployeeTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMPractice.Models;
+
+namespace MVVMPracticeTests.ModelTests {
+    [TestClass]
+    public class EmployeeTests {
+        [TestMethod]
+        public void TestEmployee() {
+            Employee emp = new Employee();
+
+            Assert.IsNotNull(emp.ID);
+        }
+    }
+}
