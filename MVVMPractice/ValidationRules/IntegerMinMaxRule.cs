256948fe56ccbe0b323ccbc7235ca847dbe576d9
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-10 11:05:03 +0200

    Add Validation Rule to verify that input is an integer between 2 values
    Add unit tests for verifying the validation

---
+++ MVVMPractice/ValidationRules/IntegerMinMaxRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace MVVMPractice.ValidationRules {
+    public class IntegerMinMaxRule : ValidationRule {
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            int integerValue = 0;
+            try {
+                if (((string)value).Length > 0) integerValue = int.Parse((string)value);
+            } catch (Exception e) {
+                return new ValidationResult(false, $"Illegal characters or {e.Message}");
+            }
+
+            if (integerValue < Min || integerValue > Max) {
+                return new ValidationResult(false, $"Please enter a value in the range: {Min}-{Max}.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ MVVMPracticeTests/ValidationRuleTests/IntegerMinMaxRuleTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMPractice.ValidationRules;
+
+namespace MVVMPracticeTests.ValidationRuleTests {
+    [TestClass]
+    public class IntegerMinMaxRuleTests {
+        [TestMethod]
+        public void TestValidate() {
+            IntegerMinMaxRule minMaxRule = new IntegerMinMaxRule() { Min = 20, Max = 100 };
+
+            ValidationResult result = minMaxRule.Validate(50, CultureInfo.InvariantCulture);
+            Assert.IsFalse(result.IsValid);
+
+            result = minMaxRule.Validate("10", CultureInfo.InvariantCulture);
+            Assert.IsFalse(result.IsValid);
+
+            result = minMaxRule.Validate("110", CultureInfo.InvariantCulture);
+            Assert.IsFalse(result.IsValid);
+
+            result = minMaxRule.Validate(true, CultureInfo.InvariantCulture);
+            Assert.IsFalse(result.IsValid);
+
+            result = minMaxRule.Validate("50", CultureInfo.InvariantCulture);
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
