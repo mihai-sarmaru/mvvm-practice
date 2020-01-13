c9e0108a77e8ddbc3edcb153246850ba36a19d83
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-13 14:18:09 +0200

    Add NotEmptyRule to TextBoxes
    Add unit tests to NotEmptyRule

---
+++ MVVMPractice/ValidationRules/NotEmptyRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace MVVMPractice.ValidationRules {
+    public class NotEmptyRule : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            if (value == null || ((string)value).Length == 0) return new ValidationResult(false, $"Field cannot be empty.");
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ MVVMPracticeTests/ValidationRuleTests/NoteEmptyRuleTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMPractice.ValidationRules;
+
+namespace MVVMPracticeTests.ValidationRuleTests {
+    [TestClass]
+    public class NoteEmptyRuleTests {
+        [TestMethod]
+        public void TestValidate() {
+            NotEmptyRule minMaxRule = new NotEmptyRule();
+
+            ValidationResult result = minMaxRule.Validate(null, CultureInfo.InvariantCulture);
+            Assert.IsFalse(result.IsValid);
+
+            result = minMaxRule.Validate("", CultureInfo.InvariantCulture);
+            Assert.IsFalse(result.IsValid);
+
+            result = minMaxRule.Validate("Hello", CultureInfo.InvariantCulture);
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
