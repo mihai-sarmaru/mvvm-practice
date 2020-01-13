502bf2dc7b9b72b5f603dec41228d58016d613b1
Mihai Sarmaru <mihai.sarmaru@napa.fi>
2020-01-13 13:48:31 +0200

    Add EmployeeFormConverter to remove 0 int binding
    Add unit tests to the Converter

---
+++ MVVMPractice/Converters/EmployeeFormConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MVVMPractice.Converters {
+    public class EmployeeFormConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ConvertValues(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ConvertValues(value);
+        }
+
+        private object ConvertValues(object value) {
+            if (value is string) return value;
+            return ((int)value == 0) ? string.Empty : value;
+        }
+    }
+}
---
+++ MVVMPracticeTests/ConverterTests/EmployeeFormConverterTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMPractice.Converters;
+
+namespace MVVMPracticeTests.ConverterTests {
+    [TestClass]
+    public class EmployeeFormConverterTests {
+        [TestMethod]
+        public void TestConvert() {
+            EmployeeFormConverter converter = new EmployeeFormConverter();
+            Assert.AreEqual(7, (int)converter.Convert(7, typeof(int), null, CultureInfo.InvariantCulture));
+            Assert.AreEqual(string.Empty, (string)converter.Convert(0, typeof(int), null, CultureInfo.InvariantCulture));
+            Assert.AreEqual("hello", converter.Convert("hello", typeof(int), null, CultureInfo.InvariantCulture));
+        }
+    }
+}
