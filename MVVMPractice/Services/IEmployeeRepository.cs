0da6638cca55f338608c4f0b33ca24165cae9e62
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-15 15:46:49 +0200

    Add IEmployeeRepository interface

---
+++ MVVMPractice/Services/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using MVVMPractice.Models;
+using System.Collections.Generic;
+
+namespace MVVMPractice.Services {
+    public interface IEmployeeRepository {
+        List<Employee> GetEmployeeList();
+        void SaveEmployeeToList(Employee employee);
+        void RemoveEmployeeFromList(Employee employee);
+    }
+}
