765b98de410ea3e8b6bfa625427398a6b3166e0b
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-15 17:06:24 +0200

    Add ContainerHelper singleton class to register types to Unity

---
+++ MVVMPractice/Containers/ContainerHelper.cs
@@ -0,0 +1,14 @@
+﻿using MVVMPractice.Services;
+using Unity;
+using Unity.Lifetime;
+
+namespace MVVMPractice.Containers {
+    public static class ContainerHelper {
+        public static IUnityContainer Container { get; private set; }
+
+        static ContainerHelper() {
+            Container = new UnityContainer();
+            Container.RegisterType<IEmployeeRepository, EmployeeRepository>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
